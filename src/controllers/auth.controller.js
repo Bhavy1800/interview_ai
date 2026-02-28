89e411fe021cb34a0b199c49ce6f6a7f6e279581
Bhavy Patel <bhavyapatel1800@gmail.com>
2026-03-01 00:01:49 +0530

    register user controller added -Bhavy

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,58 @@
+const userMode = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+/**
+ * @name registerUserController
+ * @description Register a new user expoects username, email and password in the request body
+ * @access public
+ */
+async function registerUserController(req, res) {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide username, email and password" });
+  }
+
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  /* isUserAlreadyExists.username == username */
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "Account already exist with this username or email address",
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User register successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+
+module.exports = { registerUserController };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,3 +1,14 @@
 // const express = require("express")
 const {Router} = require("express")
+const authController = require("../controllers/auth.controller")
+
 const authRouter =  Router()
+
+/** js docs
+ * @route POST /api/auth/register
+ *  @description Register a new user
+ * @access public
+ */
+authRouter.post("/register",authController.registerUserController)
+
+module.exports = authRouter
